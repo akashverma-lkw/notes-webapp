56d38ef61a09ea5a2be9709601187a3676e25498
Akash Verma <akashvermastp7@gmail.com>
2025-07-30 21:23:14 +0530

    Fix: Added required packages and types for deployment

--- backend/src/routes/noteRoutes.ts
+++ backend/src/routes/noteRoutes.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { createNote, getNotes, deleteNote } from "../controllers/noteController";
+import express, { RequestHandler } from "express";
 import { protect } from "../middlewares/authMiddleware";
+import { createNote, getNotes, deleteNote } from "../controllers/noteController";
 
 const router = express.Router();
 
-router.post("/", protect, createNote);
-router.get("/", protect, getNotes);
-router.delete("/:id", protect, deleteNote);
+router.post("/", protect as RequestHandler, createNote as RequestHandler);
+router.get("/", protect as RequestHandler, getNotes as RequestHandler);
+router.delete("/:id", protect as RequestHandler, deleteNote as RequestHandler);
 
 export default router;
